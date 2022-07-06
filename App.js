1da5e8cbb718440d6f3ee1dadbd6a76afed0172c
Erik Murminacho <erik.murminacho@grupobusiness.it>
2022-07-05 19:11:04 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button,Alert } from 'react-native';
 
 export default function App() {
+
+  const Separator = () => <View style={styles.separator} />;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Pases</Text> 
+      <View style={styles.buttonContainer}>
+        <Button style={styles.container} title="Activas" color={"#b8bb26"} />
+        <Separator/>
+        <Button title="Expiradas" color={"#9c3f3c"} />
+      </View>
+      <View style={styles.paseContainer}>
+      <Text>Fecha Compra:</Text>
+      <Text>Fecha Expiracion:</Text>
+      <Text>Pases Restantes:</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +24,35 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: "flex",
+    marginTop:"10%",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  paseContainer:{
+    display: "flex",
+    alignContent: "space-between",
+    width:'85%',
+    borderRadius: 20,
+    padding: "10%",
+    backgroundColor: "#47d08e"
+  },
+  buttonContainer:{
+    flexDirection: "row",
+    margin:"5%",
+    paddingHorizontal:"10%",
+    paddingVertical:"3%",
+    alignItems: "stretch",
+    backgroundColor:"#ebdbb2",
+    borderRadius: 15
+  },
+  titulo:{
+    marginBottom:"3%",      
+    fontSize:50,
+  },
+  separator:{
+    marginHorizontal:10,
+  }
+  
 });
