8808b1b8de86106d6fe2ca01bb5eaf8b07c29e51
Alan Josueth Pacheco Cueva <alan.cueva@yahoo.es>
2022-07-06 00:07:59 -0500

    Expiradas

--- components/Home.js
+++ components/Home.js
@@ -12,10 +12,6 @@ export default function Home({ navigation,route }) {
     navigation.navigate('Login');
   }
 
-  function verExpiradas(){
-    navigation.navigate('Expiradas');
-  }
-
   return (
     <>
        <View style={styles.salirContainer}>
@@ -32,7 +28,7 @@ export default function Home({ navigation,route }) {
             color={"#b8bb26"}
           />
           <Separator />
-          <Button title="Expiradas" color={"#9c3f3c"} onPress={()=>verExpiradas} />
+          <Button title="Expiradas" color={"#9c3f3c"} onPress={()=>navigation.navigate('Expiradas')} />
         </View>
 
         <View style={styles.paseContainer}>
